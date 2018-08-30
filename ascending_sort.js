8368fd9a2e10bd49f8a07b37ecd186486abbd227
Noah L <noah.luong@gmail.com>
2018-08-30 23:55:47 +0000

    ascending sort

---
+++ ascending_sort.js
@@ -0,0 +1,17 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+var result = students.sort(function(a,b) {
+  if (a.name > b.name) {
+    return 1;
+  }
+  if (a.age < b.age) {
+    return 1;
+  } else {
+    return 0;
+  }
+});
